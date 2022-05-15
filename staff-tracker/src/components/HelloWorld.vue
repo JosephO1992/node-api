9694b2b8d1ced24fb196839e9ecbaf5e65190c54
JosephO1992 <josephraoreilly@gmail.com>
2022-05-15 17:09:50 +0100

    changed file structure and can now hit the API

---
+++ staff-tracker/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { reactive } from 'vue'; 
+const state = reactive({staff: []}); 
+
+const getUsers = async () => { 
+	console.log('function called')
+	const users = await fetch("http://localhost:8000/staff"); 
+	const res = await users.json();
+	console.log(res);
+	res.forEach((user) => { state.staff.push(user); 
+	}); 
+};
+
+function doSomething() {
+	console.log('something')
+}
+
+</script>
+
+
+
+<template>
+	<h1>Welcome to EmployeeHub.io</h1>
+
+	<button @click="getUsers" class="bg-blue-500 rounded-sm text-white p-2">Get Users</button>
+	{{ state.staff }}
+</template>
