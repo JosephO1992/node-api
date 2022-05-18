818baf6590e4b3beebe9f827af513dae86dce7d9
JosephO1992 <josephraoreilly@gmail.com>
2022-05-18 11:51:41 +0100

    added add staff page

---
+++ staff-tracker/src/components/AddUserForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <form>
+            <div class="flex flex-row w-72 justify-between items-center">
+                <label for="firstName">First Name</label>
+                <input type="text" name="firstName" id="firstName" class="border border-blue-300 rounded px-2">
+            </div>
+            <div class="flex flex-row w-72 justify-between items-center">
+                <label for="lastName">Last Name</label>
+                <input type="text" name="lastName" id="lastName" class="border border-blue-300 rounded px-2">
+            </div>
+            <div class="flex flex-row w-72 justify-between items-center">
+                <label for="age">Age</label>
+                <input type="number" name="age" id="age" class="border border-blue-300 rounded px-2">
+            </div>
+            <div class="flex flex-row w-72 justify-between items-center">
+                <label for="salary">Salary</label>
+                <input type="text" name="salary" id="salary" class="border border-blue-300 rounded px-2">
+            </div>
+            <button type="submit">Add Staff Member</button>
+        </form>
+    </div>
+</template>
--- staff-tracker/src/views/AddUser.vue
+++ staff-tracker/src/views/AddUser.vue
@@ -1,5 +1,10 @@
+<script setup>
+import AddUserForm from "../components/AddUserForm.vue";
+
+</script>
 <template>
-    <div>
-        Add a user page
+    <div class="flex flex-col items-center justify-center">
+        <h2 class="text-2xl">Add User</h2>
+        <AddUserForm />
     </div>
 </template>
