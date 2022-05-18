0047634d926bcdb0d7e166575cd0bea418d7642e
JosephO1992 <josephraoreilly@gmail.com>
2022-05-18 11:07:09 +0100

    sorted out docker-compose, added action to pinia

--- staff-tracker/src/store/userStore.js
+++ staff-tracker/src/store/userStore.js
@@ -1,28 +1,16 @@
-// import { defineStore } from "pinia";
-
-// export const userStore = defineStore('users', () => {
-//     state: () => {
-//         return {
-
-//             name: 'John Doe', 
-//             email: 'fake@email.com', 
-//             username: 'jd123'
-//         }
-
-//     }
-
-// })
-
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('userStore', {
-  // arrow function recommended for full type inference
   state: () => {
     return {
-      // all these properties will have their type inferred automatically
-      counter: 0,
-      name: 'Eduardo',
-      isAdmin: true,
+      users: []
     }
   },
+  actions: {
+      async getUsers () {
+          const users = await fetch("http://localhost:8000/staff");
+          const res = await users.json();
+          console.log(res)
+      }
+  }
 })
--- staff-tracker/src/views/Home.vue
+++ staff-tracker/src/views/Home.vue
@@ -1,18 +1,9 @@
 <script setup>
 import { reactive } from 'vue'; 
 import { useUserStore } from '../store/userStore';
-const state = reactive({staff: []}); 
+// const state = reactive({staff: []}); 
 
-const getUsers = async () => { 
-	console.log('function called')
-	const users = await fetch("http://localhost:8000/staff"); 
-	const res = await users.json();
-	console.log(res);
-	res.forEach((user) => { state.staff.push(user); 
-	}); 
-};
-
-const usersFromStore = useUserStore();
+const main = useUserStore();
 
 </script>
 
@@ -21,5 +12,5 @@ const usersFromStore = useUserStore();
 <template>
 	<h1>Welcome to EmployeeHub.io</h1>
 
-	<button @click="getUsers" class="bg-blue-500 rounded-sm text-white p-2 w-20">Get Users</button>
+	<button @click="main.getUsers" class="bg-blue-500 rounded-sm text-white p-2 w-20">Get Users</button>
 </template>
