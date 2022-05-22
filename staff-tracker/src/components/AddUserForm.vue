7c102140288059e222f85b548f345f6a9b4643aa
JosephO1992 <josephraoreilly@gmail.com>
2022-05-22 22:03:49 +0100

    can now post to the database

--- staff-tracker/src/components/AddUserForm.vue
+++ staff-tracker/src/components/AddUserForm.vue
@@ -1,25 +1,79 @@
+<script setup>
+	import { reactive } from "vue";
+
+	const newUser = reactive({
+		firstName: "",
+		lastName: "",
+		age: 0,
+		salary: 0,
+	});
+
+	const addNewUser = async () => {
+		const req = await fetch("http://localhost:8000/staff", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				firstName: newUser.firstName,
+				lastName: newUser.lastName,
+				age: newUser.age,
+				salary: newUser.salary,
+			}),
+		});
+
+		const user = await req.json();
+
+		console.log(user);
+	};
+</script>
+
 <template>
-    <div>
-        <form>
-            <div class="flex flex-row w-72 justify-between items-center mt-4">
-                <label for="firstName">First Name</label>
-                <input type="text" name="firstName" id="firstName" class="border border-blue-300 rounded px-2">
-            </div>
-            <div class="flex flex-row w-72 justify-between items-center mt-4">
-                <label for="lastName">Last Name</label>
-                <input type="text" name="lastName" id="lastName" class="border border-blue-300 rounded px-2">
-            </div>
-            <div class="flex flex-row w-72 justify-between items-center mt-4">
-                <label for="age">Age</label>
-                <input type="number" name="age" id="age" class="border border-blue-300 rounded px-2">
-            </div>
-            <div class="flex flex-row w-72 justify-between items-center mt-4">
-                <label for="salary">Salary</label>
-                <input type="text" name="salary" id="salary" class="border border-blue-300 rounded px-2">
-            </div>
-            <div class="flex justify-center items-center mt-4">
-                <button type="submit" class="bg-blue-500 text-white rounded p-2">Add Staff Member</button>
-            </div>
-        </form>
-    </div>
+	<div>
+		<form @submit.prevent="addNewUser">
+			<div class="flex flex-row w-72 justify-between items-center mt-4">
+				<label for="firstName">First Name</label>
+				<input
+					type="text"
+					name="firstName"
+					id="firstName"
+					v-model="newUser.firstName"
+					class="border border-blue-300 rounded px-2"
+				/>
+			</div>
+			<div class="flex flex-row w-72 justify-between items-center mt-4">
+				<label for="lastName">Last Name</label>
+				<input
+					type="text"
+					name="lastName"
+					id="lastName"
+					v-model="newUser.lastName"
+					class="border border-blue-300 rounded px-2"
+				/>
+			</div>
+			<div class="flex flex-row w-72 justify-between items-center mt-4">
+				<label for="age">Age</label>
+				<input
+					type="number"
+					name="age"
+					id="age"
+					v-model="newUser.age"
+					class="border border-blue-300 rounded px-2"
+				/>
+			</div>
+			<div class="flex flex-row w-72 justify-between items-center mt-4">
+				<label for="salary">Salary</label>
+				<input
+					type="text"
+					name="salary"
+					id="salary"
+					v-model="newUser.salary"
+					class="border border-blue-300 rounded px-2"
+				/>
+			</div>
+			<div class="flex justify-center items-center mt-4">
+				<button type="submit" class="bg-blue-500 text-white rounded p-2">
+					Add Staff Member
+				</button>
+			</div>
+		</form>
+	</div>
 </template>
---
+++ staff-tracker/src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="w-full h-16 flex items-center justify-center">
+		<h1 class="text-2xl text-gray-800">Welcome to EmployeeHub.io</h1>
+	</div>
+</template>
