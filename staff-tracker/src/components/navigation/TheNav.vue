3aec245af97d28ca03851064470e7981327ae960
JosephO1992 <josephraoreilly@gmail.com>
2022-06-03 00:35:47 +0100

    added auth

--- staff-tracker/src/components/navigation/TheNav.vue
+++ staff-tracker/src/components/navigation/TheNav.vue
@@ -1,8 +1,33 @@
+<script>
+	import { useAuth0 } from "@auth0/auth0-vue";
+
+	export default {
+		setup() {
+			const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
+
+			return {
+				login: () => {
+					loginWithRedirect();
+				},
+				logout: () => {
+					logout({ returnTo: window.location.origin });
+				},
+				user,
+				isAuthenticated,
+			};
+		},
+	};
+</script>
 <template>
-    <div>
-        <ul class="flex flex-row items-center justify-around">
-            <router-link to="/" class="mr-4">Home</router-link>
-            <router-link to="/add-user">Add New User</router-link>
-        </ul>
-    </div>
+	<div>
+		<ul class="flex flex-row items-center justify-around">
+			<router-link to="/" class="mr-4">Home</router-link>
+			<router-link to="/add-user">Add New User</router-link>
+			<button @click="logout" v-if="isAuthenticated">
+				Log out <code>{{ user }}</code>
+			</button>
+
+			<button @click="login" v-else>Log in</button>
+		</ul>
+	</div>
 </template>
--- staff-tracker/src/main.js
+++ staff-tracker/src/main.js
@@ -1,7 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import router from './router'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
+import router from "./router";
+import { createPinia } from "pinia";
+import { createAuth0 } from "@auth0/auth0-vue";
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App)
+	.use(router)
+	.use(
+		createAuth0({
+			domain: "dev-2b5z2q36.us.auth0.com",
+			client_id: "NVgiuB8fkPTo5cVt7G6sCdWbEgQiGWaj",
+			redirect_uri: window.location.origin,
+		})
+	)
+	.use(createPinia())
+	.mount("#app");
